729cfb60baa4d683cab24cf47b3f71a855efa335
Wojciech Kalinski <wojciech.kalinski@espeo.eu>
2018-11-26 10:15:42 +0100

    Fixes for review

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -18,49 +18,52 @@ contract RockPaperScissors {
     Shape aliceMove;
     Shape bobMove;
 
-    bytes32 encryptedAliceMove;
-    bytes32 encryptedBobMove;
+    bytes32 hashedAliceMove;
+    bytes32 hashedBobMove;
+
+    uint constant aliceIndex = 0;
+    uint constant bobIndex = 1;
 
     mapping(address => Player) public players;
 
-    event LogWinner(address player);
+    event LogWinner(uint playerIndex);
     event LogEtherDeposed(uint _amount);
 
     modifier onlyIfMovesSubmitted() {
-        require(encryptedAliceMove != bytes32(0) && encryptedBobMove != bytes32(0));
+        require(hashedAliceMove != bytes32(0) && hashedBobMove != bytes32(0));
         _;
     }
 
-    constructor(address _bob) public {
+    constructor() public {
         owner = msg.sender;
     }
 
-    function compareMoves(Shape aliceMove, Shape bobMove) private view returns (Player winner) {
+    function compareMoves(Shape aliceMove, Shape bobMove) public view returns (uint winner) {
         if (aliceMove == bobMove) {
-            revert("A tie. Nothing happens");
+            return 0;
         }
         if (aliceMove == Shape.ROCK) {
             if (bobMove == Shape.PAPER) {
-                return players[1];
+                return bobIndex;
             }
             else {
-                return players[0];
+                return aliceIndex;
             }
         }
         if (aliceMove == Shape.PAPER) {
             if (bobMove == Shape.SCISSORS) {
-                return players[1];
+                return bobIndex;
             }
             else {
-                return players[0];
+                return aliceIndex;
             }
         }
         if (aliceMove == Shape.SCISSORS) {
             if (bobMove == Shape.ROCK) {
-                return players[1];
+                return bobIndex;
             }
             else {
-                return players[0];
+                return aliceIndex;
             }
         }
         else {
@@ -68,29 +71,28 @@ contract RockPaperScissors {
         }
     }
 
-    function betGame(bytes32 _move) public payable {
+    function betGame(bytes32 _hashedMove) public payable {
         if (!hasAliceBet) {
             hasAliceBet = true;
             players[0].bet += msg.value;
             players[0].owner = msg.sender;
-            players[0].encryptedMove = _move;
+            players[0].encryptedMove = _hashedMove;
         } else {
             require(bet == msg.value);
             hasBobBet = true; 
             players[1].bet = msg.value;
             players[1].owner = msg.sender;
-            players[1].encryptedMove = _move;
+            players[1].encryptedMove = _hashedMove;
         }
     }
 
     function play() public payable onlyIfMovesSubmitted returns (bool) {
-        require(msg.sender != 0);
-        Player memory winner = compareMoves(aliceMove, bobMove);
-        emit LogWinner(winner.owner); 
+        uint gameWinner = compareMoves(aliceMove, bobMove);
+        emit LogWinner(players[gameWinner]); 
         return true;
     }
 
-    function hashPlay(address _player, string _move) private pure returns (bytes32) {
+    function hashPlay(address _player, string _move) public pure returns (bytes32) {
         bytes32 result = keccak256(abi.encodePacked(_player, _move));
         return result;
     }
